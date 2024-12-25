be1bc8f721c68a86bbd8fc3d0a0e0bacce48187c
Louis Ritz <louis.ritz12@gmail.com>
2024-12-25 19:36:19 +0100

    Message and Input added (EntryPoint.cpp)

--- EntryPoint.cpp
+++ EntryPoint.cpp
@@ -1,3 +1,9 @@
+#include <iostream> // I/o
+
+short mode;
+
 int main() {
+	cout << "Hello and welcome to Project Play, this app creates small games and mods for you.\nWhat do you want to do ?";
+	cin >> mode;
 	return 0;
 }
