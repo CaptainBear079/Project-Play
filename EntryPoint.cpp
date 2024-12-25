ee72a711d851c78141cdba67e0a07bb122e86691
Louis Ritz <louis.ritz12@gmail.com>
2024-12-25 19:41:23 +0100

    change

--- EntryPoint.cpp
+++ EntryPoint.cpp
@@ -5,5 +5,6 @@ short mode;
 int main() {
 	cout << "Hello and welcome to Project Play, this app creates small games and mods for you.\nWhat do you want to do ?";
 	cin >> mode;
+	if(mode == 1) {}
 	return 0;
 }
