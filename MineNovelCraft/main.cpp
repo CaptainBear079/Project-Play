1fe472ee99de48f7c7e88ac577a1745d13f03cee
Louis Ritz <louis.ritz12@gmail.com>
2024-12-30 17:24:06 +0100

    change

---
+++ MineNovelCraft/main.cpp
@@ -0,0 +1,43 @@
+#include <Windows.h>
+
+// DEF
+HWND hMain_Menu;
+// Window Code
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR args, int nCmdShow) {
+	WNDCLASSW WndClassConfig = { 0 };
+
+	WndClassConfig.hbrBackground = (HBRUSH)COLOR_WINDOW;
+	WndClassConfig.hCursor = LoadCursor(NULL, IDC_ARROW);
+	WndClassConfig.hInstance = hInstance;
+	WndClassConfig.lpszClassName = L"";
+	WndClassConfig.lpfnWndProc = WndProc;
+
+	if (!RegisterClassW(&WndClassConfig)) {
+		return -1;
+	}
+
+	hMain_Menu = CreateWindowW(L"", L"A BIG Disaster", WS_OVERLAPPEDWINDOW | WS_SYSMENU, 200, 100, 200, 100, NULL, NULL, hInstanceASSSS, NULL);
+	if (hMain_Menu == NULL) {
+		return -1;
+	}
+
+	MSG msg;
+	while (GetMessageW(msg, NULL, 0, 0)) {
+		TranslateMessage(&msg);
+		DispatchMessageW(&msg);
+	}
+	return 0;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+	switch (msg) {
+	case WM_DESTROY: {
+		PostQuitMessage(0);
+	} break;
+	default: {
+		return DefWindowProcW(hWnd, msg, wParam, lParam);
+	}
+	}
+}
