1e65bfcafc48b2afb735115d0997555182d44229
Louis Ritz <louis.ritz12@gmail.com>
2025-02-16 18:59:04 +0100

    Version 0.1.5

--- main.cpp
+++ main.cpp
@@ -4,11 +4,12 @@ char mode; // Mode of the program
 
 int main() {
 	std::cout << "Hello and welcome to Project Play.\n" << std::endl;
-	std::cout << "-------------| Input" << std::endl
-		      << "Mod Creator  | 1   Coming soon!" << std::endl
-		      << "Game Creator | 2   Coming soon!" << std::endl
-		      << "";
-	std::cout << "What do you want to do ?";
+	std::cout << "--------------| Input" << std::endl
+		      << "Mod Creator   | 1     Coming soon!" << std::endl
+		      << "Game Creator  | 2     Coming soon!" << std::endl
+		      << "Save and Exit | 3" << std::endl
+		      << "Exit          | 4" << std::endl;
+	std::cout << "What do you want to do ?: ";
 	std::cin >> mode;
 	if(mode == '1') {}
 	return 0;
